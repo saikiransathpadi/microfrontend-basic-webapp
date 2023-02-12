67b6433fccef809f034220f3b5d57b1f1e4df06f
Sathpadi SaiKiran <sathpadisaikiran@gmail.com>
2023-02-12 11:52:46 +0530

    minor change

--- packages/dashboard/src/components/Dashboard.vue
+++ packages/dashboard/src/components/Dashboard.vue
@@ -1,27 +1,24 @@
 <template>
   <div class="p-grid p-fluid dashboard">
-    <div>
-
-      <div class="p-col-12 p-lg-4">
-        <div class="card summary">
-          <span class="title">Users</span>
-          <span class="detail">Number of visitors</span>
-          <span class="count visitors">12</span>
-        </div>
+    <div class="p-col-12 p-lg-4">
+      <div class="card summary">
+        <span class="title">Users</span>
+        <span class="detail">Number of visitors</span>
+        <span class="count visitors">12</span>
       </div>
-      <div class="p-col-12 p-lg-4">
-        <div class="card summary">
-          <span class="title">Sales</span>
-          <span class="detail">Number of purchases</span>
-          <span class="count purchases">534</span>
-        </div>
+    </div>
+    <div class="p-col-12 p-lg-4">
+      <div class="card summary">
+        <span class="title">Sales</span>
+        <span class="detail">Number of purchases</span>
+        <span class="count purchases">534</span>
       </div>
-      <div class="p-col-12 p-lg-4">
-        <div class="card summary">
-          <span class="title">Revenue</span>
-          <span class="detail">Income for today</span>
-          <span class="count revenue">$3,200</span>
-        </div>
+    </div>
+    <div class="p-col-12 p-lg-4">
+      <div class="card summary">
+        <span class="title">Revenue</span>
+        <span class="detail">Income for today</span>
+        <span class="count revenue">$3,200</span>
       </div>
     </div>
 
